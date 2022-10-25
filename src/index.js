39f13650644ef7a0f0d60fca3c720d8f3bdf2818
Dmitriy Shumeikin <bunchomuncho@gmail.com>
2022-10-26 00:37:25 +0300

    Sprint_1 для второй проверки

--- src/index.js
+++ src/index.js
@@ -30,21 +30,3 @@ loginPageButton.onclick = () => page.innerHTML = loginPage
 signinPageButton.onclick = () => page.innerHTML = signinPage
 profilePageButton.onclick = () => page.innerHTML = profilePage
 chatsPageButton.onclick = () => page.innerHTML = chats
-
-// if (window.location.href === `${baseUrl}/chats`) {
-//   page.innerHTML = chats
-// } else if (window.location.href === `${baseUrl}/404`) {
-//   page.innerHTML = notFoundErrorPage
-// } else if (window.location.href === `${baseUrl}/500`) {
-//   page.innerHTML = internalServerErrorPage
-// } else if (window.location.href === `${baseUrl}/login`) {
-//   page.innerHTML = loginPage
-// } else if (window.location.href === `${baseUrl}/signin`) {
-//   page.innerHTML = signinPage
-// } else if (window.location.href === `${baseUrl}/profile`) {
-//   page.innerHTML = profilePage
-// } else if (window.location.href === `${baseUrl}/edit-password`) {
-//   page.innerHTML = editPasswordPage
-// } else if (window.location.href === `${baseUrl}/edit-profile`) {
-//   page.innerHTML = editProfilePage
-// }
--- src/pages/chats/index.js
+++ src/pages/chats/index.js
@@ -1,8 +1,5 @@
 import template from './chats.hbs'
 
-const chats = template({
-  title: 'Тут скоро будет страница с чатами',
-  subtitle: 'А пока можно изучить другие страницы нажимая кнопки в верхнем меню'
-})
+const chats = template({})
 
 export { chats }
